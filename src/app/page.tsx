9b19971131f33608b068783c47c335b65980f7c8
.env <chauhantushar6706@gmail.com>
2026-01-13 20:17:09 +0530

    tRPC setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import prisma from '@/lib/db'
-const page = async () => {
+import { trpc, getQueryClient } from "@/trpc/server"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
+import Client from "./client"
+import { Suspense } from "react"
 
-  const posts = await prisma.post.findMany();
-  return (
-    <div className=''>
-      {JSON.stringify(posts,null,2)}
+
+
+const page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text: "Explain the theory of relativity in simple terms."}))
+  return (  
+    <HydrationBoundary state={dehydrate(queryClient)}>
+    <div className='bg-black text-white'>
+      <Suspense fallback={<div>Loading...</div>}>
+      <Client/>
+      </Suspense>
     </div>
+    </HydrationBoundary>
   )
 }
 
